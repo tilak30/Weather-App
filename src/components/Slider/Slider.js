e1cd05a54972f655f2549226eba736189bf43fa5
Tilak <tilakbhansali3009@gmail.com>
2021-06-21 20:50:59 +0530

    Search-bar added

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import './Slider.css';
 //import location from '../../images/my_location.svg';
 import clouds from '../../images/Cloud-background.png';
 import sun from '../../images/Shower.png';
 import location2 from '../../images/place.svg';
+import close from '../../images/close.svg';
+import searchicon from '../../images/search.svg';
+import go from '../../images/chevron_right.svg'
+
+
 function Slider () {
-    return (
-        <div className="main">
+    const [open,setOpen] = useState(false);
+    if(!open){
+        return(
             <div className="aside">
-                <nav className="aside_nav">
-                    <button className="btn btn-secondary button1">Search for places</button>
+                <nav className="aside-nav">
+                    <button onClick={() => setOpen(true)} className="btn btn-secondary button1">Search for places</button>
                 </nav>
                 <div className="background">
                     <img className="clouds" src={clouds}></img>
@@ -32,8 +38,35 @@ function Slider () {
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else{
+        return (
+            <div className="search-bar">
+                <div className="cross-icon">
+                    <button onClick={() => setOpen(false)}><img src={close} alt="close-icon"/></button>
+                </div>
+                <div class="search-container">
+                    <img src={searchicon } alt="search" width="17.05" height="17.06" />
+                    <input type="text" class="search-input" placeholder="search location" />
+                    <input type="button" class="search-button" value="Search" />
+                </div>
+                <div className="allcities">
+                    <div className="search-cities">
+                        <span type="button" className="city-name">London</span>
+                        <img src={go} alt="go"/>
+                    </div>
+                    <div className="search-cities">
+                        <span type="button" className="city-name">New York</span>
+                        <img src={go} alt="go"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+
+    
 }
 
 export default Slider;
