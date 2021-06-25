b0d7203c5a4f812019280ca3f824eb84065900aa
Tilak <tilakbhansali3009@gmail.com>
2021-06-25 18:49:55 +0530

    Slider functionality added :tada:

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,69 +4,77 @@ import './Slider.css';
 import clouds from '../../images/Cloud-background.png';
 import sun from '../../images/Shower.png';
 import location2 from '../../images/place.svg';
-import close from '../../images/close.svg';
+import close123 from '../../images/close123.svg';
 import searchicon from '../../images/search.svg';
 import go from '../../images/chevron_right.svg'
 
 
 function Slider () {
     const [open,setOpen] = useState(false);
-    if(!open){
+
         return(
-            <div className="aside">
-                <nav className="aside-nav">
-                    <button onClick={() => setOpen(true)} className="btn btn-secondary button1">Search for places</button>
-                </nav>
-                <div className="background">
-                    <img className="clouds" src={clouds}></img>
-                    <img className="sun" src={sun}></img>
-                </div>
-                <div className="temperature">
-                    <span className="number">15</span>
-                    <span className="quan">℃</span>
-                </div>
-                <h3 className="status">Clear</h3>
-                <div className="bottom">
-                    <div className="date">
-                        <span>Today</span>
-                        <span className="dot">•</span>
-                        <span>Sat, 19 Jun</span>
-                    </div>
-                    <div className="location2">
-                        <img src={location2} width="24" height="24" />
-                        <span>New Delhi</span>
-                    </div>
-                </div>
-            </div>
+            <div>
+            {   open 
+                ?<SideNavOpen setOpen={setOpen} />
+                :<SideNavClosed setOpen={setOpen}/>
+        }    
+        </div>
         );
-    }
-    else{
-        return (
-            <div className="search-bar">
-                <div className="cross-icon">
-                    <button onClick={() => setOpen(false)}><img src={close} alt="close-icon"/></button>
-                </div>
-                <div class="search-container">
-                    <img src={searchicon } alt="search" width="17.05" height="17.06" />
-                    <input type="text" class="search-input" placeholder="search location" />
-                    <input type="button" class="search-button" value="Search" />
-                </div>
-                <div className="allcities">
-                    <div className="search-cities">
-                        <span type="button" className="city-name">London</span>
-                        <img src={go} alt="go"/>
-                    </div>
-                    <div className="search-cities">
-                        <span type="button" className="city-name">New York</span>
-                        <img src={go} alt="go"/>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-
-    
 }
 
 export default Slider;
+
+function SideNavOpen({setOpen}){
+    return (
+        <div className="search-bar">
+            <button type="button" onClick={() => setOpen(false)} className="close cross-icon" style={{backgroundColor: "#1E213A", color: "#E7E7EB"}}>
+                <span style={{fontSize: "17px" ,fontWeight: 1000}} ariaHidden="true">&times;</span>
+            </button>
+            <div class="search-container">
+                <img src={searchicon } alt="search" width="17.05" height="17.06" />
+                <input type="text" class="search-input" placeholder="search location" />
+                <input type="button" class="search-button" value="Search" />
+            </div>
+            <div className="allcities">
+                <div className="search-cities">
+                    <span type="button" className="city-name">London</span>
+                    <img src={go} alt="go"/>
+                </div>
+                <div className="search-cities">
+                    <span type="button" className="city-name">New York</span>
+                    <img src={go} alt="go"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function SideNavClosed({setOpen}){
+    return (
+        <div className="aside">
+            <nav className="aside-nav">
+                <button onClick={() => setOpen(true)} className="btn btn-secondary button1">Search for places</button>
+            </nav>
+            <div className="background">
+                <img className="clouds" src={clouds}></img>
+                <img className="sun" src={sun}></img>
+            </div>
+            <div className="temperature">
+                <span className="number">15</span>
+                <span className="quan">℃</span>
+            </div>
+            <h3 className="status">Clear</h3>
+            <div className="bottom">
+                <div className="date">
+                    <span>Today</span>
+                    <span className="dot">•</span>
+                    <span>Sat, 19 Jun</span>
+                </div>
+                <div className="location2">
+                    <img src={location2} width="24" height="24" />
+                    <span>New Delhi</span>
+                </div>
+            </div>
+        </div>
+    );
+}
